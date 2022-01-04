589c9b01d1bd372958c32711a3f74184f7c59dcb
TKDuff <94256721+TKDuff@users.noreply.github.com>
2022-01-04 23:38:49 +0000

    Update LinkedListQueue.java

--- LinkedListQueue.java
+++ LinkedListQueue.java
@@ -1,87 +1,131 @@
+import java.security.MessageDigest;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.*;
 public class LinkedListQueue {
 
 	public static void main(String[] args) {
-		
+		Dictionary dict = new Dictionary();
 		Scanner in = new Scanner(System.in);
-		int num = Integer.parseInt(in.nextLine());
-		Link[] array = new Link[num];
 		
-		for(int i = 0; i < num; i++) {
-			array[i] = new Link(in.nextLine());
-		}
+		
+		long startTime = System.currentTimeMillis();
 
+		QueueLinkedList myList = new QueueLinkedList();
 		
-		for(int i = 0; i < num; i++) {
-			int select = i;
-			int previous = i-1;
-			int next = i+1;
-			if(previous != -1) {
-				array[select].previous = array[previous];
+		final int totalWord = 37907; 		//word count of each book
+		int numLines = 0;
+		
+		
+		String sentence = ""; 		
+		int countStart = 0;				
+		for(int i = 0; i < totalWord; i++) {						
+			String current = dict.getWord(i);	
+			if(current.charAt(current.length()-1) == '.' ) {	
+				if(countStart >= 3 && countStart <= 10) {		
+					myList.insert(sha256(sentence + current));
+				}
+				sentence = "";									
+				countStart = 0;
+			} else {											
+				sentence += current + " ";						
 			}
-			if(next != num) {
-				array[select].next=array[next];
-			}
-		}
+			countStart++;
+			numLines++;
+		}	
 		
-		LinkedList myList = new LinkedList();
 		
-		if(num>0) {
-			myList.first=array[0];
-			myList.last=array[num-1];
-		}
-		
-		myList.insertTail("Test");
-		
+		int highest = 0;
+		String outer = "", inner = "";
 		Link current = myList.first;
-		
-		for(int i = 0; i < num+1; i++) {
-			System.out.println(current.data +" ");
+		while(current != null) {
+			Link compared = myList.first.next;		//ensure not comparing same sha
+			while(compared != null) {
+				int comp = compare(current.data, compared.data);
+				if(comp > highest && comp != 64) {
+					highest = comp;
+					outer = current.data;
+					inner = compared.data;
+				}
+				compared = compared.next;
+			}
 			current = current.next;
+			myList.remove();
 		}
 		
-	}
+		System.out.println(highest + " " + outer + " " + inner);
 		
+		
+		
+		long endTime   = System.currentTimeMillis();
+		NumberFormat formatter = new DecimalFormat("#0.00000");
+			System.out.print("Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds");
+
+	}
+	
+	private static int compare(String s1, String s2) {
+    	int count = 0;
+    	int pos = 0;
+    	while(pos <= 63) {
+    		if ( s1.charAt(pos)  == s2.charAt(pos)) {
+    			count++;
+    		}
+    		pos++;
+    	}
+    	return count;
+    	
+	}
+	
+	public static String sha256(String input){
+        try{
+            MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+            byte[] salt = "CS210+".getBytes("UTF-8");
+            mDigest.update(salt);
+            byte[] data = mDigest.digest(input.getBytes("UTF-8"));
+            StringBuffer sb = new StringBuffer();
+            for (int i=0;i<data.length;i++){
+                sb.append(Integer.toString((data[i]&0xff)+0x100,16).substring(1));
+            }
+            return sb.toString();
+        }catch(Exception e){
+            return(e.toString());
+        }
+    }
+	
 }
 
 class Link{
 	public String data;
 	public Link next;
-	public Link previous;
 	
 	public Link(String input) {
 		data = input;
 		next = null;
-		previous =null;
 	}
-	
-	
 }
 
-class LinkedList{
+class QueueLinkedList{
 	public Link first;
 	public Link last;
 	
-	public LinkedList() {
+	public QueueLinkedList() {
 		first = null;
 	}
 	
-	public boolean isEmpty() {
-		return (first==null);
-	}
-	
-	
-	public void removeHead() {
+	public void remove() {
 		first = first.next;
 	}
 	
-	public void insertTail(String data) {
+	public void insert(String data) {
 		Link newLink = new Link(data); 		//create new link
-		last.next = newLink;			//old last link point to new link
-		last = newLink;				//update last link object to point to new node
+		if(first == null) {
+			first = newLink;
+			last = newLink;
+		} else {
+		last.next = newLink;				//old last link point to new link
+		last = newLink;						//update last link object to point to new node
 		
-		
-	}
-	
+		}
+	}	
 }
 
